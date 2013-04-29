1dd20bdc6d27c71885a8cf1ad2bd4737ec09be1e
Matt Richard <matthew.w.richard@gmail.com>
2013-04-29 15:09:38 -0600

    Find avahi-common library

--- zeroconf_avahi/cmake/FindAvahi.cmake
+++ zeroconf_avahi/cmake/FindAvahi.cmake
@@ -10,14 +10,18 @@
 #  AVAHI_LIBRARY_DIRS
 
 find_path(AVAHI_INCLUDE_DIR avahi-common/defs.h)
-find_library(AVAHI_LIBRARY 
+find_library(AVAHI_CLIENT_LIBRARY
                   NAMES avahi-client
                   HINT ${AVAHI_LIBDIR} ${AVAHI_LIBRARY_DIRS})
-set(AVAHI_LIBRARIES ${AVAHI_LIBRARY})   
+find_library(AVAHI_COMMON_LIBRARY
+                  NAMES avahi-common
+                  HINT ${AVAHI_LIBDIR} ${AVAHI_LIBRARY_DIRS})
+set(AVAHI_LIBRARIES ${AVAHI_CLIENT_LIBRARY} ${AVAHI_COMMON_LIBRARY})
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Avahi DEFAULT_MSG
-				  AVAHI_LIBRARY
+				  AVAHI_CLIENT_LIBRARY
+				  AVAHI_COMMON_LIBRARY
                                   AVAHI_INCLUDE_DIR)
-mark_as_advanced(AVAHI_INCLUDE_DIR AVAHI_LIBRARY)
+mark_as_advanced(AVAHI_INCLUDE_DIR AVAHI_CLIENT_LIBRARY AVAHI_COMMON_LIBRARY)
 
 
